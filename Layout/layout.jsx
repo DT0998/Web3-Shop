7c8ab9a6121c27a386916f6123c2847610bb6862
DT0998 <duongkytrieuvn@gmail.com>
2022-05-26 23:18:08 +0700

    update search bar

--- Layout/layout.jsx
+++ Layout/layout.jsx
@@ -1,15 +1,19 @@
 import { Grid } from "@mui/material";
 import React from "react";
+import SearchBar from "./Search-bar/Search-bar";
 import SlideBar from "./Slidebar/slidebar";
-
+import classes from './layout.module.css'
 function Layout({ children }) {
   return (
     <React.Fragment>
       <Grid container spacing={2}>
-      <Grid item xl={4}>
+      <Grid item xs={3}>
         <SlideBar />
       </Grid>
-      <Grid item xl={8}>
+      <Grid item xs={9}>
+        <div className={classes.MenuRight_container}>
+        <SearchBar/>
+        </div>
         <main>{children}</main>
       </Grid>
       </Grid>
