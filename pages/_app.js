9633fdfa1f469c75eed09e7f77cc1ee3e4643110
DT0998 <duongkytrieuvn@gmail.com>
2022-05-26 20:51:52 +0700

    update web3

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from '../components/Layout/layout';
 import {MetamaskProvider} from '../store';
-import GoogleAnalytics from './GoogleAnalytics';
 
 function MyApp({ Component, pageProps }) {
   
@@ -10,10 +9,6 @@ function MyApp({ Component, pageProps }) {
     <MetamaskProvider>
     <Layout>
       <Component {...pageProps} />
-      {
-        process.env.NODE_ENV === 'production' &&
-          <GoogleAnalytics />
-      }
     </Layout>
     </MetamaskProvider>
     ) 
