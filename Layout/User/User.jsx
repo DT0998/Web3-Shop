4cceee64e0913f0e6d411149123b4f42b437051a
DT0998 <duongkytrieuvn@gmail.com>
2022-05-29 16:01:29 +0700

    update icon

--- Layout/User/User.jsx
+++ Layout/User/User.jsx
@@ -6,7 +6,7 @@ import Wallet from "./Wallet";
 function User(props) {
   return (
     <React.Fragment>
-      <Box display="flex" alignItems="center" gap="20px">
+      <Box display="flex" alignItems="center" gap="40px">
         {props.type === "UserIconNav" && <Notification />}
         <div className={classes.User_icon}></div>
         {props.type === 'UserIconNav' && <Wallet/>}
