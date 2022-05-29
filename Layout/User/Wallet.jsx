7e951a4b1bb46d20cb24218cad278e4ea7c30df7
DT0998 <duongkytrieuvn@gmail.com>
2022-05-29 15:58:53 +0700

    update wallet

--- Layout/User/Wallet.jsx
+++ Layout/User/Wallet.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { FaWallet } from "react-icons/fa";
+import classes from "./Wallet.module.css";
 function Wallet() {
-    return ( 
-        <React.Fragment>
-        <FaWallet/>
-        </React.Fragment>
-     );
+  return (
+    <React.Fragment>
+      <FaWallet className={classes.Wallet_icon} />
+      
+    </React.Fragment>
+  );
 }
 
 export default Wallet;
