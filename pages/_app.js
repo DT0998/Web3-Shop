ccef1c5ea681e2934ce7c7f37505dfd33204a349
DT0998 <duongkytrieuvn@gmail.com>
2022-06-23 21:24:52 +0700

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,9 @@
 import '../styles/globals.css'
 import {MetamaskProvider} from '../store';
 import Layout from '../Layout/layout';
-import { useEffect } from 'react';
-import Aos from "aos";
-import "aos/dist/aos.css";
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    Aos.init()
-  }, [])
+
   
   return(
     <MetamaskProvider>
