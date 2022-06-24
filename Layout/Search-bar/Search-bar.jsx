379fc0733b6dcca8fa92b25f845fdcfd168be097
DT0998 <duongkytrieuvn@gmail.com>
2022-06-25 00:58:00 +0700

    update nav mobile

--- Layout/Search-bar/Search-bar.jsx
+++ Layout/Search-bar/Search-bar.jsx
@@ -1,21 +1,14 @@
 import React from "react";
 import classes from "./Search-bar.module.css";
 import { FaSearch } from "react-icons/fa";
-import { FaBars } from "react-icons/fa";
 import { Box } from "@mui/system";
 
-import useMediaQuery from "../../hook/useMediaquery";
-function SearchBar(props) {
-  // hook
-  const isMobile = useMediaQuery("(max-width:1024px)");
+
+function SearchBar() {
   return (
     <React.Fragment>
       <Box display="flex" alignItems="center">
-        {isMobile &&
-        <div className={classes.bar} onClick={props.onOpen}>
-          <FaBars/>
-        </div>
-        }
+       
         <div className={classes.SearchBar_container}>
           <FaSearch className={classes.SearchBar_icon} />
           <input placeholder="Search..." className={classes.SearchBar_input} />
--- Layout/User/User.jsx
+++ Layout/User/User.jsx
@@ -3,8 +3,11 @@ import React from "react";
 // import Notification from "./Notifications";
 import classes from "./User.module.css";
 import Wallet from "./Wallet";
-
+import { FaBars } from "react-icons/fa";
+import useMediaQuery from "../../hook/useMediaquery";
 function User(props) {
+    // hook
+    const isMobile = useMediaQuery("(max-width:1024px)");
   return (
     <React.Fragment>
       <Box display="flex" alignItems="center" gap="40px">
@@ -15,6 +18,11 @@ function User(props) {
             <Wallet />
           </div>
         )}
+        {isMobile &&
+        <div className={classes.bar} onClick={props.onOpen}>
+          <FaBars/>
+        </div>
+        }
       </Box>
     </React.Fragment>
   );
